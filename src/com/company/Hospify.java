86bce775525947c2ef38614331b13ce9816743eb
Ugo <ugo.prenat@gmail.com>
2020-12-09 14:48:15 +0100

    Création de playlist

--- src/com/company/Hospify.java
+++ src/com/company/Hospify.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Hospify {
@@ -45,32 +46,25 @@ public class Hospify {
     }
 
     private static void creerPlaylist() {
-        ArrayList<ArrayList<String>> musiques = new ArrayList<>();
-        ArrayList<String> musique1 = new ArrayList<String>();
-        ArrayList<String> musique2 = new ArrayList<String>();
-        ArrayList<String> musique3 = new ArrayList<String>();
-        ArrayList<String> musique4 = new ArrayList<String>();
+        System.out.print("Entrer le nom de la playlist : ");
+        Scanner scan = new Scanner(System.in);
+        String nomPlaylist = scan.nextLine();
 
-        musique1.add("Musqiue 1 titre");
-        musique1.add("Musqiue 1 auteur");
-        musique1.add("1:45");
+        ArrayList<ArrayList<HashMap<String, String>>> listePlaylist = new ArrayList<>();
+        ArrayList<HashMap<String, String>> playlist1 = new ArrayList<>();
+        HashMap<String, String> musique1 = new HashMap<String, String>();
 
-        musique2.add("Musqiue 2 titre");
-        musique2.add("Musqiue 2 auteur");
-        musique2.add("2:45");
 
-        musique3.add("Musqiue 3 titre");
-        musique3.add("Musqiue 3 auteur");
-        musique3.add("3:45");
+        musique1.put("titre", "titre chanson 1");
+        musique1.put("auteur", "auteur chanson 1");
+        musique1.put("duree", "1:45");
 
-        musique4.add("Musqiue 4 titre");
-        musique4.add("Musqiue 4 auteur");
-        musique4.add("4:45");
+        // musique2.put(...)
+        // ...
 
-        musiques.add(musique1);
-        musiques.add(musique2);
-        musiques.add(musique3);
-        musiques.add(musique4);
+        playlist1.add(musique1);
+        listePlaylist.add(playlist1);
+
     }
 
 
