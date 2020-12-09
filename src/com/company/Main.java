7570b8ae5c7f2926a31476b9fbbbe306110701a7
Yanis <yanisjdz@outlook.fr>
2020-12-09 13:38:42 +0100

    Menu au lancement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static String entree;
+
+
     public static void main(String[] args) {
+
+        System.out.println(" ");
+        System.out.println("~~~~~~~~~~~~~ Bienvenue sur Laspit'app ~~~~~~~~~~~~~ ");
+        System.out.println(" ");
+        System.out.println("Bonjour, Laspit'app vous propose actuellement 2 fonctionnalités:");     //A changer quand on aura les classes
+        System.out.println(" ");
+        System.out.println("1- Hospify : Un lecteur de musiques.");                                 //pour afficher une liste de fonctionnalités
+        System.out.println("2- Colossal Save : Un super jeu d'aventure!");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Que désirez vous faire? Veuillez faire votre choix. ");
+        System.out.println("Vous pouvez quittez l'application en tapant la lettre Q ");
+
+        entree = entreeUtilisateur();
+
+        if(entree.equals("1")) {
+        }
+
         ArrayList<ArrayList<String>> musiques = new ArrayList<>();
         ArrayList<String> musique1 = new ArrayList<String>();
         ArrayList<String> musique2 = new ArrayList<String>();
@@ -34,4 +57,24 @@ public class Main {
 
         LecteurMusique.lecteurMusique(musiques);
     }
+
+
+
+    static String entreeUtilisateur(){
+
+        entree = scanner.nextLine();
+
+        while (!entree.equals("1") && !entree.equals("2") && !entree.toLowerCase().equals("q")){
+            System.out.println(" ");
+            System.out.println("Veuillez faire un choix parmi les fonctionnalités proposées.");
+            entreeUtilisateur();
+        }
+        if (entree.toLowerCase().equals("q")){
+            System.out.println(" ");
+            System.out.println("Au revoir, à bientôt");
+            System.exit(0);
+        }
+        return entree;
+    }
+
 }
