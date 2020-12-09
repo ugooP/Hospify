e2175c885b7c88f636b3456977a3be09f3241125
Ugo <ugo.prenat@gmail.com>
2020-12-09 14:20:56 +0100

    Début Hospify.java

--- src/com/company/Hospify.java
+++ src/com/company/Hospify.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hospify {
@@ -11,25 +12,73 @@ public class Hospify {
     public void affichageMenu() {
 
         System.out.println(" ");
-        System.out.println("~~~~~~~~~~~~~ Hospify ~~~~~~~~~~~~~ ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~ Hospify ~~~~~~~~~~~~~~~~~~~~~ ");
         System.out.println(" ");
+        System.out.println("1- Jouer une playlist");
+        System.out.println("2- Créer une playlist");
+        System.out.println("3- Rechercher une chanson");
+        System.out.println("4- Retour au menu principal");
         System.out.println(" ");
-        System.out.println("1- Lecture aléatoire"); //Lecture des chansons du fichier .txt de façon aléatoire
-        System.out.println("2- Mes playlist");      //Gestion de playlist
-        System.out.println("3- Retour au menu principal. ");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println("Que désirez vous faire? Veuillez faire votre choix. ");
+        System.out.println("Que désirez vous faire ? Veuillez faire votre choix");
         System.out.println("Vous pouvez quittez l'application en tapant la lettre Q ");
 
         entree = entreeUtilisateur();
+
+        if (entree.equals("1")) {
+            afficherPlaylist();
+        }
+        else if (entree.equals("2")) {
+            creerPlaylist();
+        }
+        else if (entree.equals("3")) {
+            rechercherChanson();
+        }
+        else if (entree.equals("4")) {
+            Main.mainMenu();
+        }
     }
+
+    private void afficherPlaylist() {
+    }
+
+    private void rechercherChanson() {
+    }
+
+    private static void creerPlaylist() {
+        ArrayList<ArrayList<String>> musiques = new ArrayList<>();
+        ArrayList<String> musique1 = new ArrayList<String>();
+        ArrayList<String> musique2 = new ArrayList<String>();
+        ArrayList<String> musique3 = new ArrayList<String>();
+        ArrayList<String> musique4 = new ArrayList<String>();
+
+        musique1.add("Musqiue 1 titre");
+        musique1.add("Musqiue 1 auteur");
+        musique1.add("1:45");
+
+        musique2.add("Musqiue 2 titre");
+        musique2.add("Musqiue 2 auteur");
+        musique2.add("2:45");
+
+        musique3.add("Musqiue 3 titre");
+        musique3.add("Musqiue 3 auteur");
+        musique3.add("3:45");
+
+        musique4.add("Musqiue 4 titre");
+        musique4.add("Musqiue 4 auteur");
+        musique4.add("4:45");
+
+        musiques.add(musique1);
+        musiques.add(musique2);
+        musiques.add(musique3);
+        musiques.add(musique4);
+    }
+
 
     static String entreeUtilisateur(){
 
         entree = scanner.nextLine();
 
-        while (!entree.equals("1") && !entree.equals("2") && !entree.equals("3") && !entree.toLowerCase().equals("q")){
+        while (!entree.equals("1") && !entree.equals("2") && !entree.equals("3") && !entree.equals("4") && !entree.toLowerCase().equals("q")){
             System.out.println(" ");
             System.out.println("Veuillez faire un choix parmi les fonctionnalités proposées.");
             entreeUtilisateur();
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,38 +13,9 @@ public class Main {
 
         mainMenu();
 
-
-        ArrayList<ArrayList<String>> musiques = new ArrayList<>();
-        ArrayList<String> musique1 = new ArrayList<String>();
-        ArrayList<String> musique2 = new ArrayList<String>();
-        ArrayList<String> musique3 = new ArrayList<String>();
-        ArrayList<String> musique4 = new ArrayList<String>();
-
-        musique1.add("Musqiue 1 titre");
-        musique1.add("Musqiue 1 auteur");
-        musique1.add("1:45");
-
-        musique2.add("Musqiue 2 titre");
-        musique2.add("Musqiue 2 auteur");
-        musique2.add("2:45");
-
-        musique3.add("Musqiue 3 titre");
-        musique3.add("Musqiue 3 auteur");
-        musique3.add("3:45");
-
-        musique4.add("Musqiue 4 titre");
-        musique4.add("Musqiue 4 auteur");
-        musique4.add("4:45");
-
-        musiques.add(musique1);
-        musiques.add(musique2);
-        musiques.add(musique3);
-        musiques.add(musique4);
-
-        LecteurMusique.lecteurMusique(musiques);
     }
 
-    private static void mainMenu() {
+    public static void mainMenu() {
         System.out.println(" ");
         System.out.println("~~~~~~~~~~~~~ Bienvenue sur Laspit'app ~~~~~~~~~~~~~ ");
         System.out.println(" ");
@@ -61,7 +32,6 @@ public class Main {
 
         if(entree.equals("1")) {
             Hospify hospify = new Hospify();
-
             hospify.affichageMenu();
         }
     }
