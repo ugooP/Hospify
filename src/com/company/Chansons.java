7cf71489f58d7f0560c345f0130a514cd3a1548e
Yanis <yanisjdz@outlook.fr>
2020-12-10 10:09:41 +0100

    fonction tabChansons + Affichage

--- src/com/company/Chansons.java
+++ src/com/company/Chansons.java
@@ -1,38 +1,41 @@
 package com.company;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class Chansons {
 
-    public static void afficherChanson() throws FileNotFoundException {
+    public static void afficherChanson(String[] chansons)  {
+
+
+        for (int i = 0; i < chansons.length; i++) {
+            if (chansons[i] != null) {
+                System.out.println(i + ". " + chansons[i]);
+            }
+        }
+    }
+
+    public static void tabChansons(String[] chansons) throws FileNotFoundException {
         Scanner scanner = null;
         int indexChanson = 0;
-        Scanner file = new Scanner(new BufferedReader(new FileReader("test.txt")));
-
         try {
-            scanner = new Scanner(new BufferedReader(new FileReader("test.txt")));
+            scanner = new Scanner(new BufferedReader(new FileReader("musiques.txt")));
 
             while (scanner.hasNextLine()) {
                 String musique = scanner.nextLine();
-                System.out.println(indexChanson + ". " + ligne);
+                chansons[indexChanson] = musique;
                 indexChanson++;
             }
             scanner.close();
+
         }catch (FileNotFoundException e){
             System.out.println("Fichier introuvable");
             e.printStackTrace();
             System.exit(0);
         }
-    }
-    public static void nouvelleChanson(String titre, String auteur, String duree) {
 
-    }
-
-    public static void rechercherChanson() {
     }
 
     public static void lireChanson() {
