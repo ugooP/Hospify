4ce4761de031d85a54966059ee6e8dcb1dd853ba
Yanis <yanisjdz@outlook.fr>
2020-12-11 09:06:37 +0100

    Class Musiques

---
+++ src/com/company/Musiques.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Musiques {
+
+    private String artiste;
+    private String titre;
+    private String duree;
+
+    public Musiques(String titre, String artiste, String duree) {
+        this.titre = titre;
+        this.artiste = artiste;
+        this.duree = duree;
+    }
+
+    public Musiques() {
+
+    }
+
+    public void setArtiste(String artiste) {
+        this.artiste = artiste;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setDuree(String duree) {
+        this.duree = duree;
+    }
+
+    public String getArtiste() {
+        return artiste;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getDuree() {
+        return duree;
+    }
+
+
+    public static void lectureFichier(){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("musiques2.txt")));
+            scanner.useDelimiter("/");
+            while (scanner.hasNextLine()){
+                String titre = scanner.next();
+
+                scanner.skip(scanner.delimiter());
+
+                String artiste = scanner.next();
+
+                scanner.skip(scanner.delimiter());
+
+                String duree = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                Musiques musiques = new Musiques(titre,artiste,duree);
+                System.out.println(musiques.getTitre() + " - " + musiques.getArtiste() + " - " +musiques.getDuree());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
